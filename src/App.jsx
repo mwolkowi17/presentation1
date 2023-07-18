36141de3674d74f78cc1305334c2f2f8549fc860
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2023-07-18 09:02:30 +0200

    working sample

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 //import React, { useRef, useState } from 'react'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Plane1 } from './Plane'
 //import { OrbitControls } from '@react-three/drei'
 import { Box2 } from './Box2'
-import {useLoader } from '@react-three/fiber'
+import { useLoader } from '@react-three/fiber'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import { Suspense } from 'react'
 
 // function Box(props) {
 //   // This reference will give us direct access to the mesh
@@ -31,23 +32,26 @@ import { TextureLoader } from 'three/src/loaders/TextureLoader'
 //   )
 // }
 
-function App(){
-  const [pictSource,pictSourceSet]=useState('hero3.jpg')
-  const colorMap = useLoader(TextureLoader, pictSource)
-  
-  return(
-  <Canvas>
-    <ambientLight />
-    <pointLight position={[10, 10, 10]} />
-    
-  {/* <Box position={[1.2, 0, 0]} /> 
-  <Box position={[1.2, 0, 2]} />  */}
-    <Plane1 position={[0, 0, -2]} map={colorMap} />
-    {/* <Plane1 position={[0,-3,2]} />
-    <OrbitControls /> */}
-    <Box2 position={[5,0,0]} action={(event) => pictSourceSet('hero8.jpg')} />
+function App() {
+  const [pictSource, pictSourceSet] = useState(true)
+  const colorMap = useLoader(TextureLoader, (pictSource ? 'hero3.jpg' : 'hero8.jpg'))
 
-  </Canvas>
+
+
+  return (
+    <Canvas>
+      <Suspense fallback={null}>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+
+        {/* <Box position={[1.2, 0, 0]} /> 
+  <Box position={[1.2, 0, 2]} />  */}
+        <Plane1 position={[0, 0, -2]} map={colorMap} />
+        {/* <Plane1 position={[0,-3,2]} />
+    <OrbitControls /> */}
+        <Box2 position={[5, 0, 0]} action={(event) => pictSourceSet(false)} />
+      </Suspense>
+    </Canvas>
   )
 }
 
