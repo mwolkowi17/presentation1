bc61665202aac9ff3733161403c337eb94916e07
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2023-07-20 22:55:33 +0200

    next step

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,20 @@ import { imagelist } from './Collection'
 
 
 function App() {
+  let n=0
   const colorMap2 = useLoader(TextureLoader,imagelist[0])
   const colorMap3 = useLoader(TextureLoader,imagelist[1])
-
+ 
   const colorMapList = [colorMap2,colorMap3]
-  const [pictSource, pictSourceSet] = useState(colorMapList[0])
+  const [pictSource, pictSourceSet] = useState(colorMapList[n])
   //const colorMap = useLoader(TextureLoader, (pictSource ? imagelist[0] : imagelist[1]))
   
+
+  function changePicture(){
+    n=n+1;
+    pictSourceSet(colorMapList[n])
+    console.log(n)
+  }
 
   return (
     <div>
@@ -34,7 +41,8 @@ function App() {
           <Plane1 position={[0, 0, -2]} map={pictSource} />
         </Suspense>
 
-        <Box2 position={[5, 0, 0]} action={(event) => pictSourceSet(colorMapList[1])} />
+        {/* <Box2 position={[5, 0, 0]} action={(event) => pictSourceSet(colorMapList[1])} /> */}
+        <Box2 position={[5, 0, 0]} action={changePicture} />
 
       </Canvas>
     </div>
